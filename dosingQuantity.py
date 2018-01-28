6cc9a3806be6230207788a4e1ec1c2351097db93
auxo <auxo_86@hotmail.com>
2018-01-29 00:38:40 +0800

    改變need_amps -> need_units

--- dosingQuantity.py
+++ dosingQuantity.py
@@ -7,8 +7,8 @@ dosing_interval_hrs = 6
 med_contain = 1500
 each_time_dosage = 1600
 dosing_duration_days = 5
-need_amps = 0
-# 每支藥品可以投予幾次。
+need_units = 0
+# 每單位藥品可以投予幾次
 valid_dosing_times_per_amp = math.floor(med_contain / each_time_dosage)
 each_time_amps = math.floor(each_time_dosage / med_contain * 10) / 10
 '''
@@ -25,11 +25,11 @@ if valid_extent_times >= 1:  # 表示可以累計
     if (valid_dosing_times_per_amp - 1) >= valid_extent_times:
         # 可以延一次就是可以用兩次，可以延兩次就是可以用三次
         effectiive_amp_content = each_time_dosage * (valid_extent_times + 1)
-        need_amps = 24 * dosing_duration_days / dosing_course * dosing_times * each_time_dosage / effectiive_amp_content
+        need_units = 24 * dosing_duration_days / dosing_course * dosing_times * each_time_dosage / effectiive_amp_content
     else:
-        need_amps = 24 * dosing_duration_days / dosing_course * dosing_times * each_time_dosage / med_contain
+        need_units = 24 * dosing_duration_days / dosing_course * dosing_times * each_time_dosage / med_contain
 else:
-    need_amps = 24 * dosing_duration_days / dosing_course * dosing_times * math.ceil(each_time_dosage / med_contain)
+    need_units = 24 * dosing_duration_days / dosing_course * dosing_times * math.ceil(each_time_dosage / med_contain)
 
-need_amps = math.ceil(need_amps)
-print(f'共需要 {need_amps} 支')
+need_units = math.ceil(need_units)
+print(f'共需要 {need_units} 支')
